eec43153e5bf9d298d18ece532be4eac623bae56
Kah919 <kahyap13@gmail.com>
2019-11-09 15:10:28 -0500

    change button style to styled-components

--- src/components/mail.js
+++ src/components/mail.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import styled, { css } from 'styled-components'
 
 export default props => {
+
+    const Button = styled.button`
+        padding: 10px 20px 10px 20px;
+        background-color: white;
+        border-radius: 5px;
+        margin-top: 2%;
+    `;
+
     return(
         <section className="mailing__container">
             <h1> {props.header} </h1>
             <p> {props.text} </p>
 
-            <button> {props.button.text} </button>
+            <Button> {props.button.text} </Button>
         </section>
     )
 }
-
-
-// change into function
-// function until class is needed
