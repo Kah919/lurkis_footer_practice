97f1492e2d43861db0e8562f37f8341d40414ed7
Kah919 <kahyap13@gmail.com>
2019-11-15 16:06:41 -0500

    incorporate background gif

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
+import Background from '../images/nelsonmandela.jpg';
+import Background2 from '../images/nelson.gif';
+
 
 
 export default props => {
 
     const Section = styled.section`
         height: 100vh;
-        width: 60%;
-        margin: 0 auto;
         display: flex;
         align-items: center;
-        background-image: url("paper.gif");
+        background-image: url(${Background2});
+        background-size: cover;
+        color: white;
+        z-index: -1;
+        opacity: 0.9;
+        border: 5px solid black;
+    `;
+
+    const Div = styled.div`
+        width: 70%;
+        margin: 0 auto;
+        color: black;
+        padding: 2em;
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.4);
+        border: 2px solid black;
     `;
 
     const H1 = styled.h1`
@@ -22,7 +38,7 @@ export default props => {
     `;
 
     const H4 = styled.h4`
-        margin-bottom:  1em;
+        margin-bottom: 1.5em;
     `;
 
     const P = styled.p`
@@ -32,7 +48,7 @@ export default props => {
 
     return(
         <Section>
-            <div>
+            <Div>
                 <Fade left>
                 <H1> { props.header } </H1>
                 </Fade>
@@ -44,7 +60,7 @@ export default props => {
                 <Zoom delay={1000}>
                     <P> { props.text } </P>
                 </Zoom>
-            </div>
+            </Div>
         </Section>
     )
 }
